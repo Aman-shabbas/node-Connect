7cf3803c09117f0583009f8e7f4d637fa45b372b
Aman Shabbas <83913651+Aman-shabbas@users.noreply.github.com>
2025-03-01 18:09:33 +0530

    added tutorial

--- script.js
+++ script.js
@@ -9,21 +9,20 @@ const initialiseLine = (startX, startY) => {
   line.setAttribute("x2", startX);
   line.setAttribute("y1", startY);
   line.setAttribute("y2", startY);
-  line.setAttribute("stroke", "black");
-  line.setAttribute("stroke-width", "2");
+  line.classList.add("line");
   svg.appendChild(line);
 
   return line;
 };
 
-const addNode = (x, y) => {
-  const point = document.createElement("div");
-  point.classList.add("node");
-  point.style.left = `${x}px`;
-  point.style.top = `${y}px`;
-  body.appendChild(point);
+const placeNode = (x, y) => {
+  const node = document.createElement("div");
+  node.classList.add("node");
+  node.style.left = `${x}px`;
+  node.style.top = `${y}px`;
+  body.appendChild(node);
 
-  return point;
+  return node;
 };
 
 const joinPoints = (line, end) => {
@@ -35,8 +34,33 @@ const getDistance = (from, to) => {
   return Math.sqrt(Math.pow(to.x - from.x, 2) + Math.pow(to.y - from.y, 2));
 };
 
-document.addEventListener("click", (event) => {
-  const node = addNode(event.clientX, event.clientY);
+const placeGuideNode = (x, y) => {
+  const node = placeNode(x, y);
+  node.classList.add("guide-point");
+  return node;
+};
+
+const clearPoints = () => {
+  if (points.length === 0) return;
+  const node = points.pop().node;
+  body.removeChild(node);
+  clearPoints();
+};
+
+const clearLines = () => {
+  for (const line of svg.querySelectorAll(".line")) {
+    svg.removeChild(line);
+  }
+};
+
+const clearGuidNodes = (guideNodes) => {
+  for (const node of guideNodes) {
+    body.removeChild(node);
+  }
+};
+
+const addNode = (event) => {
+  const node = placeNode(event.clientX, event.clientY);
 
   for (const point of points) {
     if (
@@ -61,7 +85,7 @@ document.addEventListener("click", (event) => {
     y: event.clientY,
     lines: [line],
   });
-});
+};
 
 document.onmousemove = (event) => {
   for (const point of points) {
@@ -81,4 +105,77 @@ document.onmousemove = (event) => {
   }
 };
 
-document.onload = alert("try clicking and moving the mouse in the window..");
+const enableUserMode = (event) => {
+  const hitText = body.querySelector("p");
+  hitText.style.display = "none";
+  addNode(event);
+  document.addEventListener("click", addNode);
+  document.removeEventListener("click", enableUserMode);
+};
+
+const endTutorial = (guideNodes, hintText) => {
+  clearPoints();
+  clearLines();
+  clearGuidNodes(guideNodes);
+  guideNodes.clear();
+  hintText.textContent =
+    "now click anywhere on the screen to start creating your own shapes";
+  document.addEventListener("click", enableUserMode);
+};
+
+const tutorialThirdStep = (guideNodes, hintText) => {
+  const node = placeGuideNode(660, 420);
+  guideNodes.add(node);
+  hintText.textContent = "click on the last circle to form a triangle";
+
+  node.onclick = () => {
+    addNode({ clientX: 660, clientY: 420 });
+    node.classList.remove("guide-point");
+    const hint = "move the mouse away from the point to see the line break";
+    hintText.textContent = hint;
+    const coundown = document.createElement("p");
+    body.appendChild(coundown);
+    let counter = 5;
+    const timerId = setInterval(() => {
+      coundown.textContent = `tutorial will end in ${counter--}`;
+    }, 1 * 1000);
+    setTimeout(() => {
+      body.removeChild(coundown);
+      clearInterval(timerId);
+      endTutorial(guideNodes, hintText);
+    }, 6 * 1000);
+  };
+};
+
+const tutorialSecondStep = (guideNodes, hintText) => {
+  const node = placeGuideNode(630, 260);
+  guideNodes.add(node);
+  hintText.textContent = "now click on the new green circle";
+
+  node.onclick = () => {
+    addNode({ clientX: 630, clientY: 260 });
+    node.classList.remove("guide-point");
+    tutorialThirdStep(guideNodes, hintText);
+  };
+};
+
+const tutorialFirstStep = (guideNodes) => {
+  const node = placeGuideNode(500, 380);
+  guideNodes.add(node);
+
+  const hintText = body.querySelector("p");
+  hintText.textContent = "click on the green circle";
+
+  node.onclick = () => {
+    addNode({ clientX: 500, clientY: 380 });
+    node.classList.remove("guide-point");
+    tutorialSecondStep(guideNodes, hintText);
+  };
+};
+
+const initiliseTutorial = () => {
+  const guideNodes = new Set();
+  tutorialFirstStep(guideNodes);
+};
+
+window.onload = initiliseTutorial;
